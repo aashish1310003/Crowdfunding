07fe0aba0600d5e1253a3967b5635fcb75ad7aeb
Aashish <aashish.cs21@bitsathy.ac.in>
2025-03-25 13:24:55 +0530

    updated the  futures

--- FRONT-END/Nish/crowdfunding/src/api/api.js
+++ FRONT-END/Nish/crowdfunding/src/api/api.js
@@ -1,5 +1,6 @@
 // const BASE_URL = "https://195t12z1-8080.inc1.devtunnels.ms";
 const BASE_URL = "https://crowdfundings-production.up.railway.app";
+// const BASE_URL = "https://195t12z1-8080.inc1.devtunnels.ms";
 // const BASE_URL = "http://localhost:8080";
-// iuhiuhui
+
 export { BASE_URL };
